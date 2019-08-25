158232cb4d31ad25b66cf672bcc1664027f40510
Haoran Feng <harrifeng@gmail.com>
2019-08-25 16:09:35 +0800

    new envs

--- envs.sh
+++ envs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # export DATABASE_URL="postgres://postgres@localhost:5432/saleor"
-# export SECRET_KEY="irrelevant"
+export SECRET_KEY="irrelevant"
 # export DIFF_RESULTS_BASE_URL="http://dhrwmpu5reeyd.cloudfront.net"
 export QUERIES_RESULTS_PATH=/tmp/queries-results.json
